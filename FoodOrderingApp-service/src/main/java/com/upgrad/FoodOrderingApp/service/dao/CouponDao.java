2e8a5e042181e8fd47906d3fd4f26af5c9b5f0b7
Sai Darshan <saidarshan27@gmail.com>
2021-06-07 22:12:00 +0530

    finish OrderController

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CouponDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CouponDao.java
@@ -10,12 +10,38 @@ import javax.persistence.PersistenceContext;
 
 @Repository
 public class CouponDao {
-    @PersistenceContext
-    private EntityManager entityManager;
 
-        public CouponEntity getCouponByName(String couponName) {
+    @PersistenceContext private EntityManager entityManager;
+
+    /**
+     * This method fetches CouponEntity from database based on the coupon name.
+     *
+     * @param couponName
+     * @return CouponEntity or null if there is no coupon in database by given name.
+     */
+    public CouponEntity getCouponByName(String couponName) {
         try {
-            return entityManager.createNamedQuery("getCouponByName" , CouponEntity.class).setParameter("couponName" , couponName).getSingleResult();
+            return entityManager
+                    .createNamedQuery("couponByName", CouponEntity.class)
+                    .setParameter("couponName", couponName)
+                    .getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    /**
+     * This method fetches CouponEntity from database based on the coupon id.
+     *
+     * @param couponUUID
+     * @return CouponEntity or null if there is no coupon in database by given id.
+     */
+    public CouponEntity getCouponByCouponId(String couponUUID) {
+        try {
+            return entityManager
+                    .createNamedQuery("couponByUUID", CouponEntity.class)
+                    .setParameter("couponUUID", couponUUID)
+                    .getSingleResult();
         } catch (NoResultException nre) {
             return null;
         }
